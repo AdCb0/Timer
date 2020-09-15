f692e44a38c9b863537270ba83030f7ccd3c1c45
AdCb <55813497+AdCb0@users.noreply.github.com>
2020-09-15 17:48:34 +0300

    Update source.c

--- source.c
+++ source.c
@@ -3,6 +3,36 @@
 #include <locale.h>
 #include <time.h>
 
+/* Delphine's */
+#define RU 0
+#define EN 1
+
+#define LANGUAGE RU
+
+
+#if LANGUAGE == RU
+
+
+#define TEXT_THIS_TIMER 	"Это таймер"
+#define TEXT_ENTER_SEC 		"Введите секунды: "
+#define TEXT_ENTER_MIN		"Введите минуты: "
+#define TEXT_ENTER_HOUR 	"Введите часы: "
+#define TEXT_TIME_UP 		"Время вышло"
+
+
+#elif LANGUAGE == EN
+
+
+#define TEXT_THIS_TIMER 	"This is a timer"
+#define TEXT_ENTER_SEC 		"Enter seconds: "
+#define TEXT_ENTER_MIN 		"Enter minuts: "
+#define TEXT_ENTER_HOUR		"Enter hours: "
+#define TEXT_TIME_UP 		"Time is up"
+
+#endif
+
+
+
 struct time
 {
 	int sec, min, hour;
@@ -10,7 +40,7 @@ struct time
 
 int main()
 {	setlocale(LC_ALL, "Rus");
-printf("Это таймер\n");
+printf("%s\n",TEXT_THIS_TIMER);
 start:;
 
 char 
@@ -26,9 +56,9 @@ temp_sec = 0;
 
 
 printf("\n");
-secs: 	printf("Секунды: ");	ch2 = gets(ch1);	temp_int = atoi(ch2); 	if( temp_int>59 | temp_int<0) 	goto secs;	sec_need = sec_need + temp_int;
-mins: 	printf("Минуты: "); 	ch2 = gets(ch1); 	temp_int = atoi(ch2); 	if( temp_int>59 | temp_int<0) 	goto mins;	sec_need = sec_need + temp_int * 60;
-hours: 	printf("Часы: "); 		ch2 = gets(ch1); 	temp_int = atoi(ch2); 	if( temp_int<0) 				goto hours;	sec_need = sec_need + temp_int * 3600;
+secs: 	printf("%s",TEXT_ENTER_SEC);	ch2 = gets(ch1);	temp_int = atoi(ch2); 	if( temp_int>59 | temp_int<0) 	goto secs;	sec_need = sec_need + temp_int;
+mins: 	printf("%s",TEXT_ENTER_MIN); 	ch2 = gets(ch1); 	temp_int = atoi(ch2); 	if( temp_int>59 | temp_int<0) 	goto mins;	sec_need = sec_need + temp_int * 60;
+hours: 	printf("%s",TEXT_ENTER_HOUR); 	ch2 = gets(ch1); 	temp_int = atoi(ch2); 	if( temp_int<0) 				goto hours;	sec_need = sec_need + temp_int * 3600;
 printf("\n");
 
 sec = time(NULL);
@@ -57,7 +87,7 @@ for(;;)
 		printf("%02d:%02d:%02d	-	%02d:%02d:%02d\r",p.hour,p.min,p.sec,l.hour,l.min,l.sec);
 		if(sec >= sec_need)
 		{
-			printf("\n\n------------------------------\n\nВремя вышло\n\n------------------------------\n\a\a\a\a\a");
+			printf("\n\n------------------------------\n\n%s\n\n------------------------------\n\a\a\a\a\a",TEXT_TIME_UP);
 			goto start;
 		}
 		temp_sec = sec;
